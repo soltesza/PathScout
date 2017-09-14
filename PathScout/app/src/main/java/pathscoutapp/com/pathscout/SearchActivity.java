4eb7fe71c5a38d3ef172859ef88afb379b1074dd
Andrew Soltesz <soltesza@users.noreply.github.com>
2017-09-14 12:06:23 -0700

    Implemented rudimentary search UI

--- PathScout/app/src/main/java/pathscoutapp/com/pathscout/SearchActivity.java
+++ PathScout/app/src/main/java/pathscoutapp/com/pathscout/SearchActivity.java
@@ -1,9 +1,21 @@
 package pathscoutapp.com.pathscout;
 
+import android.database.DataSetObserver;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
 
 public class SearchActivity extends AppCompatActivity {
     public void search(View v) {
@@ -15,5 +27,6 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
     }
 }
